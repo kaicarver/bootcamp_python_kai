40630baf3097bf23245f563f8c168b78058c0e2e
Kai Carver <kaicarver@gmail.com>
2020-04-22 00:16:45 +0200

    done ex 0.2

--- day00/ex02/whois.py
+++ day00/ex02/whois.py
@@ -3,6 +3,15 @@
 import sys
 
 if len(sys.argv) == 2:
-    print("I'm Even.")
+    try:
+        if int(sys.argv[1]) == 0:
+            print("I'm Zero.")
+        elif int(sys.argv[1]) % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+    except Exception:
+        print("ERROR")
+
 elif len(sys.argv) > 2:
     print("ERROR")
