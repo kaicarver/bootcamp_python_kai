f595e915e3ab3ed0b7a8b7408938c6025f23935f
Kai Carver <kaicarver@gmail.com>
2020-04-22 22:13:27 +0200

    start ex 0.6

---
+++ day00/ex06/recipe.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+recipes = {
+    'sandwich': {
+        'ingredients': ['ham', 'bread', 'cheese', 'tomatoes'],
+        'meal': 'lunch',
+        'prep_time': 10,
+    },
+    'cake': {
+        'ingredients': ['flour', 'sugar', 'eggs'],
+        'meal': 'dessert',
+        'prep_time': 60,
+    },
+    'salad': {
+        'ingredients': ['avocado', 'arugula', 'tomatoes', 'spinach'],
+        'meal': 'lunch',
+        'prep_time': 15,
+    },
+}
+
+print(recipes)
