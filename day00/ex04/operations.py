166911b255679bdf80eb388771dbafc9b0c25ecd
Kai Carver <kaicarver@gmail.com>
2020-04-22 12:12:23 +0200

    it's a program w 2 args

---
+++ day00/ex04/operations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+
+if len(sys.argv) == 3:
+    try:
+        if int(sys.argv[1]) == 0:
+            print("I'm Zero.")
+        elif int(sys.argv[1]) % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+    except Exception:
+        print("ERROR")
+
+elif len(sys.argv) > 2:
+    print("ERROR")
+
+
