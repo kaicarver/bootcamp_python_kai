294adea92862822a5efc93d257b0e9ca4e554237
Kai Carver <kaicarver@gmail.com>
2020-04-22 14:37:43 +0200

    exceptions nearly all handled

--- day00/ex04/operations.py
+++ day00/ex04/operations.py
@@ -6,16 +6,21 @@ done = False
 
 if len(sys.argv) == 3:
     try:
-        a = int(sys.argv[1])
-        b = int(sys.argv[2])
-        (sum, diff, prod, quo, rem) = (a + b, a - b, a * b, a / b, a % b)
+        a, b = int(sys.argv[1]), int(sys.argv[2])
+        try:
+            sum, diff, prod = a + b, a - b, a * b
+            quo = a / b
+            rem = a % b
+        except Exception:
+            quo, rem = -1, -1
         print("""Sum:         {}
 Difference:  {}
 Product:     {}
 Quotient:    {}
 Remainder:   {}""".format(sum, diff, prod, quo, rem))
-        done = true
-    except Exception:
+        done = True
+    except Exception as e:
+        print(e)
         print("InputError: only numbers\n")
 
 if not done:
