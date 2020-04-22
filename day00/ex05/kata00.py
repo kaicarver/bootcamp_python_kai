cdc78d0abd825ef9ef2bee35cdf9f97475db2087
Kai Carver <kaicarver@gmail.com>
2020-04-22 15:47:03 +0200

    start ex 0.5; kata00 done

---
+++ day00/ex05/kata00.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+t = (19, 42, 21)
+
+print(t)
+
+print("The 3 numbers are: {}, {}, {}".format(*t))
---
+++ day00/ex05/test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# usage: diff <(./test.sh) test.ok.txt
+
+pycodestyle *.py
+
+python kata00.py
