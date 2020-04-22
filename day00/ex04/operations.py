af5a89c08068f44f420bfc1e13e2b343f367e9be
Kai Carver <kaicarver@gmail.com>
2020-04-22 12:22:24 +0200

    usage

--- day00/ex04/operations.py
+++ day00/ex04/operations.py
@@ -17,3 +17,8 @@ Remainder:   {}""".format(sum, diff, prod, quo, rem))
 
 elif len(sys.argv) > 2:
     print("ERROR")
+else:
+    usage = """Usage: python operations.py <number1> <number2>
+Example:
+    python operations.py 10 3"""
+    print(usage)
