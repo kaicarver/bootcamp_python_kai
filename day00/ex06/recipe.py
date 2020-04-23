9f769aaf22249b7f2d82ce1fae101b14be43e30d
Kai Carver <kaicarver@gmail.com>
2020-04-23 05:44:13 +0200

    cookbook and delete

--- day00/ex06/recipe.py
+++ day00/ex06/recipe.py
@@ -20,13 +20,36 @@ recipes = {
 
 
 def print_recipe(name):
-    print(f"""
-Recipe for {name}:
+    print(f"""Recipe for {name}:
 Ingredients list: {recipes[name]['ingredients']}
 To be eaten for {recipes[name]['meal']}.
 Takes {recipes[name]['prep_time']} minutes of cooking.
+""", end='')
+
+
+def print_cookbook():
+    print('Cookbook:')
+    for recipe in recipes.keys():
+        print_recipe(recipe)
+        print()
+
+
+def delete_cookbook(name):
+    del recipes[name]
+
+
+def prompt():
+    print("""Please select an option by typing the corresponding number:
+1: Add a recipe
+2: Delete a recipe
+3: Print a recipe
+4: Print the cookbook
+5: Quit
 """)
 
 
-for recipe in recipes.keys():
-    print_recipe(recipe)
+prompt()
+print_cookbook()
+delete_cookbook('sandwich')
+print_cookbook()
+
