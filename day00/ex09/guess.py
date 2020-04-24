f8013886560701d84c048e0904d19de3db1dbda7
Kai Carver <kaicarver@gmail.com>
2020-04-24 08:26:23 +0200

    done ex 0.9

--- day00/ex09/guess.py
+++ day00/ex09/guess.py
@@ -11,25 +11,37 @@ Good luck!
 number_prompt = """What's your guess between 1 and 99?
 >> """
 prompt = intro + number_prompt
-secret_number = str(random.randint(1, 99))
+secret_number = random.randint(1, 99)
+num_attempts = 0
+
 
 def prompt_user():
     global prompt
+    global num_attempts
     loop = False
     text = input(prompt)
+    num_attempts += 1
     if text == 'exit':
         print('Goodbye!')
-    elif text == secret_number:
-        print("""Congratulations, you've got it!
-You won in ? attempts!""")
+    elif text == str(secret_number):
+        print(f"""Congratulations, you've got it!
+You won in {num_attempts} attempts!""")
     elif text == '42':
-        print("""The answer to the ultimate question of life, the universe and everything is 42.
+        print("""The answer to the ultimate question of life, \
+the universe and everything is 42.
 Congratulations! You got it on your first try!""")
     else:
-        print("Keep trying...", secret_number)
+        try:
+            if int(text) < secret_number:
+                print("Too low!")
+            else:
+                print("Too high!")
+        except ValueError:
+            print("That's not a number.")
         prompt = number_prompt
         loop = True
     return loop
+
 
 while prompt_user():
     pass
