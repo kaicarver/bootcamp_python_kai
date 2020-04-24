07cb5aeeb2156d62277b5d3e37329abb8985bd2f
Kai Carver <kaicarver@gmail.com>
2020-04-24 10:45:41 +0200

    play with yield and iteration

--- day00/ex10/loading.py
+++ day00/ex10/loading.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
 
+from time import sleep
+
 def ft_progress(listy):
-    return list
+    n = 1
+    print('This is printed first')
+    # Generator function contains yield statements
+    yield n
+
+    n += 1
+    print('This is printed second')
+    yield n
+
+    n += 1
+    print('This is printed at last')
+    yield n
 
 listy = range(1000)
 ret = 0
 for elem in ft_progress(listy):
+    print('elem:', elem)
     ret += (elem + 3) % 5
     sleep(0.01)
 print()
