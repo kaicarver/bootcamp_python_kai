965654a2207099a9f636c43a04f8e48781848917
Kai Carver <kaicarver@gmail.com>
2020-04-24 09:58:14 +0200

    start ex 0.10

---
+++ day00/ex10/loading.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def ft_progress(listy):
+    return list
+
+listy = range(1000)
+ret = 0
+for elem in ft_progress(listy):
+    ret += (elem + 3) % 5
+    sleep(0.01)
+print()
+print(ret)
