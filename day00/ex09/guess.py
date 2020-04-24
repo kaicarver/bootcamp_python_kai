fda0f65d6bb7e81420540b6327bd404e8180a675
Kai Carver <kaicarver@gmail.com>
2020-04-24 07:34:38 +0200

    random secret number

--- day00/ex09/guess.py
+++ day00/ex09/guess.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import random
+
 intro = """This is an interactive guessing game!
 You have to enter a number between 1 and 99 to find out the secret number.
 Type 'exit' to end the game.
@@ -9,6 +11,7 @@ Good luck!
 number_prompt = """What's your guess between 1 and 99?
 >> """
 prompt = intro + number_prompt
+secret_number = str(random.randint(1, 99))
 
 def prompt_user():
     global prompt
@@ -16,14 +19,14 @@ def prompt_user():
     text = input(prompt)
     if text == 'exit':
         print('Goodbye!')
-    elif text == '5':
+    elif text == secret_number:
         print("""Congratulations, you've got it!
 You won in ? attempts!""")
     elif text == '42':
         print("""The answer to the ultimate question of life, the universe and everything is 42.
 Congratulations! You got it on your first try!""")
     else:
-        print("Keep trying...")
+        print("Keep trying...", secret_number)
         prompt = number_prompt
         loop = True
     return loop
