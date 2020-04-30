bb1ca974fc7eba425417f6a9f09acc2580c8d621
Kai Carver <kaicarver@gmail.com>
2020-04-30 07:28:35 +0200

    omg ex01 done already?

--- day01/ex01/game.py
+++ day01/ex01/game.py
@@ -2,18 +2,34 @@
 
 
 class GotCharacter:
-    def __init__(self):
-        self.first_name 
+    def __init__(self, first_name, is_alive):
+        self.first_name = first_name
+        self.is_alive = is_alive
 
 
 class Stark(GotCharacter):
+    """A class representing the Stark family. \
+Or when bad things happen to good people."""
+
     def __init__(self, first_name=None, is_alive=True):
         super().__init__(first_name=first_name, is_alive=is_alive)
         self.family_name = "Stark"
         self.house_words = "Winter is Coming"
 
-    def print_house_words:
-        pass
+    def print_house_words(self):
+        print(self.house_words)
 
-    def die:
+    def die(self):
         self.is_alive = False
+
+
+def unit_tests():
+    arya = Stark("Arya")
+    print(arya.__dict__)
+    arya.print_house_words()
+    print(arya.is_alive)
+    print(arya.__doc__)
+
+
+if __name__ == '__main__':
+    unit_tests()
