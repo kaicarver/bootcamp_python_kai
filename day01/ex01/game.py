94293b7e531a190167990b79f77388bca15714f6
Kai Carver <kaicarver@gmail.com>
2020-04-30 07:15:24 +0200

    start day 1 ex 1

---
+++ day01/ex01/game.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+class GotCharacter:
+    def __init__(self):
+        self.first_name 
+
+
+class Stark(GotCharacter):
+    def __init__(self, first_name=None, is_alive=True):
+        super().__init__(first_name=first_name, is_alive=is_alive)
+        self.family_name = "Stark"
+        self.house_words = "Winter is Coming"
+
+    def print_house_words:
+        pass
+
+    def die:
+        self.is_alive = False
