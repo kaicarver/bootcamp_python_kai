805dcf6d00eef23d71b53a0be1d41d19f603a6d8
Kai Carver <kaicarver@gmail.com>
2020-04-30 07:38:36 +0200

    just a bit more testing on ex01

--- day01/ex01/game.py
+++ day01/ex01/game.py
@@ -23,10 +23,38 @@ Or when bad things happen to good people."""
         self.is_alive = False
 
 
+class Lannister(GotCharacter):
+    """A class representing the Lannister family. \
+Or when good things happen to bad people."""
+
+    def __init__(self, first_name=None, is_alive=True):
+        super().__init__(first_name=first_name, is_alive=is_alive)
+        self.family_name = "Lannister"
+        self.house_words = "A Lannister always pays his debts"
+
+    def print_house_words(self):
+        print(self.house_words)
+
+    def die(self):
+        self.is_alive = False
+
+
+def test_class(name, family):
+    print(f"Testing character {name} of house {family}")
+
+
 def unit_tests():
+    test_class('Arya', Stark)
     arya = Stark("Arya")
     print(arya.__dict__)
     arya.print_house_words()
+    print(arya.is_alive)
+    print(arya.__doc__)
+    test_class('Cersei', Stark)
+    arya = Lannister("Cersei")
+    print(arya.__dict__)
+    arya.print_house_words()
+    arya.die()
     print(arya.is_alive)
     print(arya.__doc__)
 
