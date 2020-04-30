39a45a6567fbe46757c680b8a8783f520fe78e7d
Kai Carver <kaicarver@gmail.com>
2020-04-30 11:10:53 +0200

    a bit of generic testing

--- day01/ex02/vector.pytest.py
+++ day01/ex02/vector.pytest.py
@@ -19,8 +19,19 @@ class Vector:
         return 42
 
 
+def test_eval(exp_str):
+    label = "-> " + exp_str + ":"
+    expr = "print(" + exp_str + ")"
+    print(label)
+    eval(expr)
+
+
 if __name__ == "__main__":
     v1 = Vector([0.0, 1.0, 2.0, 3.0])
-    v2 = v1 * 5
     print(v1)
+    v2 = v1 * 5
     print(v2)
+    test_eval('Vector([0.0, 1.0, 2.0, 3.0])')
+    test_eval('Vector(3)')
+    test_eval('Vector((10, 15))')
+    test_eval('Vector([0.0, 1.0, 2.0, 3.0]) * 5')
