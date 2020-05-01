5e059e60ed84e4fbf9c31b6f497e9a93ee4834bf
Kai Carver <kaicarver@gmail.com>
2020-05-01 19:58:29 +0200

    can re-use 1.2 test.py with path hack by add of ../ex02 to sys.path

---
+++ day01/ex03/test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.insert(0, "../ex02")
+from vector import Vector
+
+
+def test_eval(exp_str):
+    label = "-> " + exp_str + ":"
+    expr = "print(" + exp_str + ")"
+    print(label)
+    eval(expr)
+    print()
+
+
+def test_op(op):
+    print(f'testing operator {op}:')
+    test_eval(f'Vector([0.0, 1.0, 2.0, 3.0]) {op} 5')
+    test_eval(f'5 {op} Vector([0.0, 1.0, 2.0, 3.0])')
+    test_eval(f'Vector([0.0, 1.0, 2.0, 3.0]) {op} 5.1')
+    test_eval(f'5.1 {op} Vector([0.0, 1.0, 2.0, 3.0])')
+    test_eval(f'"hi" {op} Vector([0.0, 1.0, 2.0, 3.0])')
+    test_eval(f'Vector([0.0, 1.0, 2.0, 3.0]) \
+{op} Vector([0.0, 1.0, 2.0, 3.0])')
+    test_eval(f'Vector([0.0, 1.0, 2.0, 3.0]) \
+{op} Vector([0.0, 1.0, 2.0])')
+
+
+if __name__ == "__main__":
+    v1 = Vector([0.0, 1.0, 2.0, 3.0])
+    print('v1', v1)
+    print('repr(v1)', repr(v1))
+    print('v1.__repr__()', v1.__repr__())
+    print(eval(repr(v1)) == v1)  # test __repr__
+    v2 = v1 * 5
+    print(v2)
+    test_eval('Vector([0.0, 1.0, 2.0, 3.0])')
+    test_eval('Vector([0.0, 1.0, 2.0, 3.0]).size')
+    test_eval('Vector(3)')
+    test_eval('Vector(1)')
+    test_eval('Vector(0)')
+    test_eval('Vector(-1)')
+    test_eval('Vector(4.2)')
+    test_eval('Vector((10, 15))')
+    test_eval('Vector((15, 10))')
+    test_eval('Vector((15, 15))')
+    test_eval('Vector((-15, 10))')
+    test_eval('Vector((10, 15.0))')
+
+    test_op('+')
+    test_op('-')
+    test_op('*')
+    test_op('/')
